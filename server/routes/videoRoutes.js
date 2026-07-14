bb0bf8fe981c9454fb9812665bd7927e3b9a4372
Rishij4 <162426055+Rishij4@users.noreply.github.com>
2026-07-14 20:15:00 +0530

    Update videoRoutes.js

--- server/routes/videoRoutes.js
+++ server/routes/videoRoutes.js
@@ -7,7 +7,10 @@ const {
   getAllVideos,
   getVideoById,
 } = require("../controllers/videoController");
-
+app.use((req, res, next) => {
+    console.log(req.method, req.url);
+    next();
+});
 router.get("/", getAllVideos);
 
 router.get("/:id", getVideoById);
