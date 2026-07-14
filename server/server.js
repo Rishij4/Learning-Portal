2f2549776ffef81b78d06c031a7dcd39e87c5249
Rishij4 <162426055+Rishij4@users.noreply.github.com>
2026-07-14 21:25:41 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,50 +1,22 @@
-console.log("********** NEW SERVER FILE LOADED **********");
 require("dotenv").config();
 
 const express = require("express");
-const cors = require("cors");
-const videoRoutes = require("./routes/videoRoutes");
-const bookmarkRoutes = require("./routes/bookmarkRoutes");
-const path = require("path");
-
-const connectDB = require("./config/db");
-
 const app = express();
 
-// Connect Database
-connectDB();
-
-// Middlewares
-app.use(cors());
-app.use(express.json());
-
-// 👇 ADD THIS HERE
-app.use((req, res, next) => {
-    console.log(req.method, req.url);
-    next();
-});
-
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-app.get("/api/test", (req, res) => {
-  res.json({
-    success: true,
-    message: "API is working"
-  });
-});
-app.use("/api/videos", videoRoutes);
-app.use("/api/bookmarks", bookmarkRoutes);
-
-// Routes
 app.get("/", (req, res) => {
-    res.json({
-        success: true,
-        message: "Learning Portal API Running..."
-    });
+  res.send("HOME WORKING");
 });
 
-// Server
+app.get("/api/test", (req, res) => {
+  res.send("API TEST WORKING");
+});
+
+app.get("/api/videos", (req, res) => {
+  res.send("VIDEOS ROUTE WORKING");
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log("Server running on", PORT);
 });
