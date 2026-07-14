ff0f58de25d96890305b3df9637d1221f744e7ad
Rishij4 <162426055+Rishij4@users.noreply.github.com>
2026-07-14 21:37:56 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,22 +1,34 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
+const videoRoutes = require("./routes/videoRoutes");
+const bookmarkRoutes = require("./routes/bookmarkRoutes");
+const path = require("path");
+
+const connectDB = require("./config/db");
+
 const app = express();
 
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+app.use("/api/videos", videoRoutes);
+app.use("/api/bookmarks", bookmarkRoutes);
+
 app.get("/", (req, res) => {
-  res.send("HOME WORKING");
-});
-
-app.get("/api/test", (req, res) => {
-  res.send("API TEST WORKING");
-});
-
-app.get("/api/videos", (req, res) => {
-  res.send("VIDEOS ROUTE WORKING");
+  res.json({
+    success: true,
+    message: "Learning Portal API Running..."
+  });
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log("Server running on", PORT);
+  console.log(`Server running on port ${PORT}`);
 });
